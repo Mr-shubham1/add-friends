81b934ac3cc203166667148f8a227c8a613aee03
Mr-shubham1 <108973912+Mr-shubham1@users.noreply.github.com>
2024-02-23 00:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
-var timer = 60;
-var score = 0;
 
 
-function runtimer(){
-    var stinterval = setInterval(() => {
-        if(timer>=0)
+function addfriend(){
+    var card = document.querySelector("#card");
+    var istatus = document.querySelector("#card h4");
+    card.addEventListener("click",function(dets){
+        console.log();
+        if(dets.target.innerHTML == "Add friend")
         {
-            document.querySelector("#Timer1").textContent = timer--;
+        istatus.innerHTML = "friends";
+        istatus.style.color = "green";
+        document.querySelector("#card .btn").innerHTML = `<button id="remove_btn">Remove</button>`;
         }
-        else
-        {
-            clearInterval(stinterval);
-            document.querySelector("#panelbottom").innerHTML = `<div id = "result"><h1>Game over </h1><h1>Score ${score} </h1></div>`;
-        }   
-    }, 1000);
-}
-
-function makebubble(){
-    var clutter = "" ;
-    for(var i =1;i<=200;i++)
-    {
-    var random = Math.floor(Math.random()*10);
-    clutter += `<div class="bubble">${random}</div>`;
-    }
-    document.querySelector("#panelbottom").innerHTML = clutter;
-}
-
-function newhit(){
-    var rnhit = Math.floor(Math.random()*10);
-    document.querySelector("#hitval").textContent = rnhit;
-    return rnhit;
-}
-
-
-function bubblehit(){
-    // event bubbleing concept
-    var scr = document.querySelector("#score1");
-    document.querySelector("#panelbottom").addEventListener("click",function(dets){
-        if(Number(dets.target.textContent)==hitvalue)
-        {
-            score += 10;
-            scr.innerHTML = score;
-            hitvalue = newhit();
-            makebubble();
-
-        }
+        
     })
-
 }
 
-makebubble();
-runtimer();
-var hitvalue = newhit();
-bubblehit();
+function removefriend(){
+    var card = document.querySelector("#card");
+    var istatus = document.querySelector("#card h4");
+    card.addEventListener("click",function(dets){
+        if(dets.target.innerHTML == "Remove")
+        {
+        istatus.innerHTML ="Stranger";
+        istatus.style.color = "red";
+        document.querySelector("#card .btn").innerHTML = `<button id="add_btn">Add friend</button>`;
+        }
+        
+    })
+}
 
 
 
+
+addfriend();
+removefriend();
